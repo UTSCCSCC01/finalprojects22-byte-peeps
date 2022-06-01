0d969312d8015a9d6dda80694ba7ccbc5cfdc7ee
Bassel Ashi <passel.y@el-ashi.net>
2022-06-01 12:59:12 -0400

    Add http util

---
+++ frontend/app/src/utils/http.ts
@@ -0,0 +1,32 @@
+const headers: { [id: string]: string } = {
+  'Content-Type': 'application/json;charset=utf-8'
+};
+
+const call = (path: string, method: string, data: Object): Promise<Response> => {
+  return fetch(`http://localhost:5000/api/v1/${path}`, { // TODO: configurable path for dev
+    method,
+    mode: 'cors', // TODO: configurable mode
+    body: data && JSON.stringify(data),
+    headers
+  }).then((response) => {
+    if (!response.ok) {
+      return Promise.reject(response);
+    }
+
+    return response;
+  });
+};
+
+export const get = (path: string, data: Object) => call(path, 'get', data);
+export const post = (path: string, data: Object) => call(path, 'post', data);
+export const patch = (path: string, data: Object) => call(path, 'patch', data);
+export const put = (path: string, data: Object) => call(path, 'put', data);
+export const del = (path: string, data: Object) => call(path, 'delete', data);
+
+export const setHeader = (key: string, value: string): void => {
+  headers[key] = value;
+};
+
+export const removeHeader = (key: string): void => {
+  delete headers[key];
+};
