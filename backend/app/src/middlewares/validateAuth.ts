7008af3cda636514e152e1371d6a3524ece7fdab
Bassel Ashi <passel.y@el-ashi.net>
2022-06-01 23:58:56 -0400

    Remove JWT middleware code

--- backend/app/src/middlewares/validateAuth.ts
+++ backend/app/src/middlewares/validateAuth.ts
@@ -1,11 +1,6 @@
 import { RequestHandler } from 'express';
 
 export const authenticateUser: RequestHandler = async (req, res, next) => {
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-
-  if (token == null) return res.sendStatus(401)
-
   // TODO: Add authentication mechanism here
 }
 
