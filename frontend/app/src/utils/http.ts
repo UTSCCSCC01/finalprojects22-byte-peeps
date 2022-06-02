24c53a23eb9998045bfacba51fa0d442cbed8620
Bassel Ashi <passel.y@el-ashi.net>
2022-06-02 11:22:07 -0400

    Integrate Axios

--- frontend/app/src/utils/http.ts
+++ frontend/app/src/utils/http.ts
@@ -1,32 +1,7 @@
-const headers: { [id: string]: string } = {
-  'Content-Type': 'application/json;charset=utf-8'
-};
+import axios from 'axios';
 
-const call = (path: string, method: string, data?: Object): Promise<Response> => {
-  return fetch(process.env.REACT_APP_BACKEND_ENDPOINT + path, {
-    method,
-    mode: process.env.REACT_APP_BACKEND_ENDPOINT == '' ? 'no-cors' : 'cors',
-    body: data && JSON.stringify(data),
-    headers
-  }).then((response) => {
-    if (!response.ok) {
-      return Promise.reject(response);
-    }
+const HTTP = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_ENDPOINT
+});
 
-    return response;
-  });
-};
-
-export const get = (path: string, data?: Object) => call(path, 'get', data);
-export const post = (path: string, data?: Object) => call(path, 'post', data);
-export const patch = (path: string, data?: Object) => call(path, 'patch', data);
-export const put = (path: string, data?: Object) => call(path, 'put', data);
-export const del = (path: string, data?: Object) => call(path, 'delete', data);
-
-export const setHeader = (key: string, value: string): void => {
-  headers[key] = value;
-};
-
-export const removeHeader = (key: string): void => {
-  delete headers[key];
-};
+export default HTTP;
