a654a2ef05912c8344f3b544a4c06b0059a9827e
Karandeep Lubana <karandeeplubana1@gmail.com>
2022-06-03 11:52:27 -0400

    BYT-27: Renamed testing file counterSlice.spec.ts to counterSlice.test.ts

--- frontend/app/src/Redux/Slices/counter/counterSlice.test.ts
+++ frontend/app/src/Redux/Slices/counter/counterSlice.test.ts
@@ -9,7 +9,7 @@ describe('counter reducer', () => {
   const initialState: CounterState = {
     value: 3,
     status: 'idle',
-  };
+  };``
   it('should handle initial state', () => {
     expect(counterReducer(undefined, { type: 'unknown' })).toEqual({
       value: 0,
