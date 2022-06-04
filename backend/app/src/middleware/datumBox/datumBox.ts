3d8dc780112428ccd9e5c91b01dd6a080a6ca86c
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-06-04 19:52:10 -0400

    BYT-28-added jsdocs to each function in the datum wrapper

--- backend/app/src/middleware/datumBox/datumBox.ts
+++ backend/app/src/middleware/datumBox/datumBox.ts
@@ -6,10 +6,21 @@ import {
   DatumServices,
 } from './datumBoxTypes';
 
-function sleep(ms: number) {
+/**
+ * Sleep function
+ * @param {number} ms
+ * @return {Promise<void>} Brief description of the returning value here.
+ */
+function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
+/**
+ * Makes a call to the datum API for the given service
+ * @param {DatumServices} service - one of the services we are using the API for
+ * @param {string} text
+ * @return {Promise<DatumCallResult>} The result of the corresponding service
+ */
 function datumCall(
   service: DatumServices,
   text: string
@@ -48,6 +59,11 @@ function datumCall(
     });
 }
 
+/**
+ * Makes multiple parallel calls to the datum API to all the services
+ * @param {string} text
+ * @return {Promise<DatumAPICallResult>} The result of all the services
+ */
 function parallelDatumCall(text: string): Promise<DatumAPICallResult> {
   const promises = parallelCalls.map((service) => {
     return datumCall(service, text);
@@ -65,9 +81,9 @@ function parallelDatumCall(text: string): Promise<DatumAPICallResult> {
 }
 
 /**
- * Makes the call for the ML API with jitter and exponential backoff retry strategy
+ * Wrapper for the parallel call for the ML API with jitter and exponential backoff retry strategy
  * @param {string} text
- * @return {[string, string, string]} Returns the sentiment, topic and subjectivity of the text
+ * @return {Promise<DatumAPICallResult>} Returns the sentiment, topic and subjectivity of the text
  * @credits https://advancedweb.hu/how-to-implement-an-exponential-backoff-retry-strategy-in-javascript/
  * @credits https://aws.amazon.com/blogs/architecture/exponential-backoff-and-jitter/
  */
@@ -87,6 +103,11 @@ async function datumBoxAPICall(
   }
 }
 
+/**
+ * The final API call wrapper for the datumBox API that developers will use
+ * @param {string} text
+ * @return {Promise<DatumAPICallResult>} Returns the sentiment, topic and subjectivity of the text
+ */
 async function DatumBoxAPICall(text: string): Promise<DatumAPICallResult> {
   return await datumBoxAPICall(text);
 }
