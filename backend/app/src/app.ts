e3fe89e6d8bfba0b3f62f6b518dcb0a43136dc61
Jiaming Yang <jiamingyang11@gmail.com>
2022-06-04 21:19:45 -0400

    BYT-40: registration api added

--- backend/app/src/app.ts
+++ backend/app/src/app.ts
@@ -7,7 +7,7 @@ import connection from './db/configs';
 import { unknownError } from './globalHelpers/globalConstants';
 import bodyParser from 'body-parser';
 import session from 'express-session';
-const users = connection.models.Users
+import { register as registrationHander } from './controllers/users';
 const app = express();
 
 const PORT = 3000;
@@ -22,23 +22,8 @@ app.use(
 
 app.use(bodyParser.json());
 
-// curl -X POST -d "username=admin&password=pass4admin" http://localhost:3000/signup/
-app.post('/signup/', function (req, res, next) {
-  // extract data from HTTP request
-  if (!('username' in req.body)) return res.status(400).end('username is missing');
-  if (!('password' in req.body)) return res.status(400).end('password is missing');
-  let username = req.body.username;
-  let password = req.body.password;
-  users.findOne({ where: { username: username } }).then((user) => {
-    console.log(user?.toJSON());
-    return res.end("hello")
-  }).catch((error) => {
-    console.error(error)
-  })
 
-}
-
-);
+app.post('/register/', ((req, res, next) => registrationHander(req, res, next)))
 
 app.use(
   (
---
+++ backend/app/src/controllers/users.ts
@@ -0,0 +1,31 @@
+import { RequestHandler } from 'express';
+const bcrypt = require('bcrypt');
+import connection from '../db/configs';
+const users = connection.models.Users
+
+export const register: RequestHandler = async function (req, res, next) {
+  // extract data from HTTP request
+  if (!('username' in req.body)) return res.status(400).end('username is missing');
+  if (!('password' in req.body)) return res.status(400).end('password is missing');
+  let username = req.body.username;
+  let password = req.body.password;
+  // check if user already exists
+  try {
+    const user = await users.findOne({ where: { username: username } })
+    if (user) return res.status(409).end("username " + username + " already exists");
+  } catch (err) {
+    return res.status(500).end(err);
+  }
+  // genearte a new salt and hash
+  bcrypt.genSalt(10, function (err: any, salt: any) {
+    bcrypt.hash(password, salt, async function (err: any, hash: any) {
+      // insert new user into the database
+      try {
+        await users.create({ username, password: hash });
+        return res.end("account created")
+      } catch (err) {
+        return res.status(500).end(err)
+      }
+    });
+  });
+}
