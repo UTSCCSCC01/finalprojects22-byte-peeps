3075b768e79e058991bf43cf1ad6b0e32359c1a8
Bassel Ashi <passel.y@el-ashi.net>
2022-06-06 11:57:19 -0400

    BYT-31: Implement miscellaneous utilities (#3)
    * added further instructions for postgres db setup for non-mac users
    
    * added port 5432 option under db instructions
    
    * Sprint 0 Marks
    
    * BYT-31: Add backend middleware
    
    * BYT-31: Add HTTP library
    
    * BYT-31: Add project logos
    
    Co-authored-by: DomiVesalius <mmillion448@gmail.com>
    Co-authored-by: Hrithik Kumar Advani <68329633+Hrithik-Kumar@users.noreply.github.com>

---
+++ backend/app/src/middlewares/validateAuth.ts
@@ -0,0 +1,7 @@
+import { RequestHandler } from 'express';
+
+export const authenticateUser: RequestHandler = async (req, res, next) => {
+  // TODO: Add authentication mechanism here
+}
+
+export default authenticateUser;
