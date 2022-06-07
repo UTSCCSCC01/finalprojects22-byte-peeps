f0a441f7260c6c9f0cc63b9205d6a5fccef309a2
Bassel Ashi <passel.y@el-ashi.net>
2022-06-06 22:07:02 -0400

    BYT-33: Restructure model definitions

--- backend/app/src/models/models.ts
+++ backend/app/src/models/models.ts
@@ -1,12 +1,8 @@
-import Todos from "./todos";
-import User from "./user/user";
-import FacebookApi from "./facebook/api";
-import FacebookPost from "./facebook/post";
-import FacebookComment from "./facebook/comment";
-import InstagramApi from "./instagram/api";
-import InstagramComment from "./instagram/comment";
-import InstagramMedia from "./instagram/media";
+import userModels from "./user/models";
+import facebookModels from "./facebook/models";
+import instagramModels from "./instagram/models";
+import youtubeModels from "./youtube/models";
 
-const models = [Todos, User, FacebookApi, FacebookPost, FacebookComment, InstagramApi, InstagramMedia, InstagramComment];
+const models = [...userModels, ...facebookModels, ...instagramModels, ...youtubeModels];
 
 export default models;
