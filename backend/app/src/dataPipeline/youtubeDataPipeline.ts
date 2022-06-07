9ec8c6358710fc6eb7ac5c527b31d5a1f1050cf1
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2022-06-06 22:15:46 -0400

    BYT-49: Created basic skeleton and docstrings for pipeline script

---
+++ backend/app/src/dataPipeline/youtubeDataPipeline.ts
@@ -0,0 +1,31 @@
+import {google} from "googleapis";
+
+/**
+ * Updates the database as follows:
+ *      1. Fetches all channel ids from db
+ *      2. Fetches all video data for all the retrieved channel ids from (1)
+ *      then updates db with any new video since the last run of this function.
+ *          - Uses YouTube Data API v3
+ *      3. Fetches all video ids from db and retrieves all comments and threads
+ *      for each video to update the db.
+ */
+async function startPipeline() {
+    updateVideos();
+    updateComments();
+}
+
+/**
+ * Uses the YouTube Data API to fetch and store information relating
+ * to all the videos belonging to all channels within the db
+ */
+function updateVideos() {
+    
+}
+
+/**
+ * Uses the YouTube Data API to fetch and store information relating
+ * to all comments under every video within the db
+ */
+function updateComments() {
+
+}
