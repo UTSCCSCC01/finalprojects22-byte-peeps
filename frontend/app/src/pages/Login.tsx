6ea0fd027793a06b1fb2e3b92503b26df7cdd161
Connie Lin <connie.lj18@gmail.com>
2022-06-08 15:13:56 -0400

    BYT-36 UI
    -update login page ui to take username and password as input (doe not save)

--- frontend/app/src/pages/Login.tsx
+++ frontend/app/src/pages/Login.tsx
@@ -1,3 +1,4 @@
+import { Button, Box, Typography, Stack, TextField, AppBar, Toolbar} from '@mui/material';
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
@@ -5,12 +6,39 @@ export interface ILoginProps {};
 
 const Login: React.FunctionComponent<ILoginProps> = (props) => {
     const login = useNavigate();
-  return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={() => login('/dashboard')}>Login</button>
-    </div>
-  )
+    return (
+        <>
+            <Box
+                component="form"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="100vh"
+                sx={{
+                boxShadow: 1,
+                borderRadius: 2,
+                p: 2,
+                minWidth: 50,
+                }}>
+                <Stack spacing={2}>
+                    <Typography component="h1" align='center'>Sign in </Typography>
+                    <TextField
+                        id="outlined-user-input"
+                        label="Username"
+                        type="username"
+                        autoComplete="current-password"
+                    />
+                    <TextField
+                        id="outlined-password-input"
+                        label="Password"
+                        type="password"
+                        autoComplete="current-password"
+                    />
+                    <Button fullWidth variant='contained' onClick={() => login('/dashboard')}>Login</Button>
+                </Stack>
+            </Box>
+        </>  
+  );
 }
 
 export default Login
