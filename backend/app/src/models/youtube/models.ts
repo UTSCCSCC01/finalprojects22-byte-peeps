c8d7a2507a6c8ceb9aafb4e71de5e032a69defd0
Bassel Ashi <passel.y@el-ashi.net>
2022-06-08 01:05:26 -0400

    BYT-33: Design database (Part 1) (#5)
    * BYT-33: Create Facebook model
    
    * BYT-33: Fix Facebook model creation
    
    * BYT-33: Add temporary user model
    
    * BYT-33: Create Facebook API model
    
    * BYT-33: Create Instagram model
    
    * BYT-33: Fix circular deppendency
    
    * BYT-33: Revert model relationship removal
    
    * BYT-33: Add data/node id
    
    * BYT-33: Add YouTube API data
    
    * BYT-33: Fix hasmany array
    
    * BYT-33: Restructure model definitions
    
    * BYT-33: Add relations in user model
    
    * BYT-33: Fix InstagramApi relation
    
    * BYT-33: Modify user model based  on requirement
    
    * BYT-33: Add numeric fields
    
    * BYT-33: Add method for retrieving number of reactions
    
    * BYT-33: Add date field for filtering
    
    * BYT-33: Rename message field to caption

---
+++ backend/app/src/models/youtube/models.ts
@@ -0,0 +1,7 @@
+import YoutubeChannel from "./channel";
+import YoutubeComment from "./comment";
+import YouTubeVideo from "./video";
+
+const youtubeModels = [YoutubeChannel, YouTubeVideo, YoutubeComment];
+
+export default youtubeModels;
