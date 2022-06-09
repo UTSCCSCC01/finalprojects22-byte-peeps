601377be56f886f3a627cbf26e1fc6af85482a20
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2022-06-08 23:00:54 -0400

    BYT-47: Created routers and routes for each YouTube model

--- backend/app/src/routes/youtube/video.ts
+++ backend/app/src/routes/youtube/video.ts
@@ -1,5 +1,14 @@
 import { Router } from "express";
+import {createVideo, deleteVideo, getAllVideos, getVideoById} from "../../controllers/youtube/video";
 
 const videoRouter = Router();
+
+videoRouter.post('/create', createVideo);
+
+videoRouter.get('/', getAllVideos);
+
+videoRouter.get('/:id', getVideoById);
+
+videoRouter.delete('/:id', deleteVideo);
 
 export default videoRouter;
