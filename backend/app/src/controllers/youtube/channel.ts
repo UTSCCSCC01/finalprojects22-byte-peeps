1280c4c2733b79cc63bd8b166ca6cc7ef1d5d689
MohammadQ <mohammad.qadir@mail.utoronto.ca>
2022-06-08 22:59:47 -0400

    BYT-47: Created layout for YouTube model controllers and implemented get request handlers for each model

--- backend/app/src/controllers/youtube/channel.ts
+++ backend/app/src/controllers/youtube/channel.ts
@@ -1,3 +1,21 @@
 import { RequestHandler } from 'express';
 import YouTubeChannel from "../../models/youtube/channel";
 
+export const deleteChannel: RequestHandler = async (req, res, next) => {
+
+};
+
+export const createChannel: RequestHandler = async (req, res, next) => {
+
+};
+
+export const getAllChannels: RequestHandler = async (req, res, next) => {
+    const allChannels: YouTubeChannel[] = await YouTubeChannel.findAll();
+    return res
+        .status(200)
+        .json({ data: allChannels });
+};
+
+export const getChannelById: RequestHandler = async (req, res, next) => {
+
+};
