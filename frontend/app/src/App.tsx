e0233bcd1fb7660f61117b6ab10375b451982a22
Jiaming Yang <jiamingyang11@gmail.com>
2022-06-10 23:55:51 -0400

    BYT64-funtionality-integration-with-ui (#17)

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Route,
-    Link,
-    BrowserRouter,
-    Routes,
-  } from "react-router-dom";
+  BrowserRouter as Router,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from 'react-router-dom';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import Surveys from './pages/Surveys';
 import ReviewApps from './pages/ReviewApps';
@@ -16,20 +17,21 @@ import Header from './Components/Header';
 export interface IAppProps {}
 
 const App: React.FunctionComponent<IAppProps> = (props) => {
-    return (
+  return (
     <>
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Login />}/>
-                <Route path="dashboard" element={<Dashboard />}/>
-                <Route path="surveys" element={<Surveys />}/>
-                <Route path="reviews" element={<ReviewApps />}/>
-                <Route path="socials" element={<SocialMedia />}/>
-            </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="surveys" element={<Surveys />} />
+          <Route path="reviews" element={<ReviewApps />} />
+          <Route path="socials" element={<SocialMedia />} />
+        </Routes>
+      </BrowserRouter>
     </>
-    );
+  );
 };
 
 export default App;
--- frontend/app/src/pages/Dashboard.tsx
+++ frontend/app/src/pages/Dashboard.tsx
@@ -1,29 +1,42 @@
-import React from 'react'
+import React from 'react';
 import { experimentalStyled as styled } from '@mui/material/styles';
-import { useNavigate } from 'react-router-dom'
-import {Box, Grid, Paper} from '@mui/material' 
+import { useNavigate } from 'react-router-dom';
+import { Box, Grid, Paper } from '@mui/material';
 
-export interface IDashProps {};
+export interface IDashProps {}
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 const Dashboard: React.FunctionComponent<IDashProps> = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <>
-      <h1>Dashboard</h1>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <h1>Dashboard</h1>
+        <button onClick={() => navigate('/')}>log out</button>
+      </div>
       {/* <button onClick={() => navigate('/dashboard')}>Dashboard</button>
       <button onClick={() => navigate('/surveys')}>Surveys</button>
       <button onClick={() => navigate('/reviews')}>Review Apps</button>
       <button onClick={() => navigate('/socials')}>Social Media</button> */}
-      <Box sx={{ flexGrow: 1 , p:2}}>
-        <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Box sx={{ flexGrow: 1, p: 2 }}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
           {Array.from(Array(6)).map((_, index) => (
             <Grid item xs={2} sm={4} md={4} key={index}>
               <Item>charts/stats, to be added</Item>
@@ -32,7 +45,7 @@ const Dashboard: React.FunctionComponent<IDashProps> = (props) => {
         </Grid>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
