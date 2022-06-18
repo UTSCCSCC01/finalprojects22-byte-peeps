c436a1d297d267fa0aa29d3566604b6574fb1e39
Bassel Ashi <passel.y@el-ashi.net>
2022-06-18 03:50:27 -0400

    BYT-103: Implement drop down for pages

--- frontend/app/src/Components/FacebookLoginWrapper/FacebookLoginWrapper.tsx
+++ frontend/app/src/Components/FacebookLoginWrapper/FacebookLoginWrapper.tsx
@@ -1,45 +1,125 @@
+import { Alert, Button, CircularProgress, FormControl, Grid, InputLabel, MenuItem, Select, Snackbar } from '@mui/material';
+import SaveIcon from '@mui/icons-material/Save';
 import { useEffect } from 'react';
 import FacebookLogin from 'react-facebook-login';
 import { useAppDispatch, useAppSelector } from '../../Redux/hooks';
-import { getActiveAsync, selectStatus } from '../../Redux/Slices/facebookLoginWrapper/facebookLoginWrapperSlice';
+import {
+  getSettingsAsync,
+  saveCurrentPageAsync,
+  selectCurrentPage,
+  selectDisplaySettingsSaved,
+  selectPages,
+  selectSaveButtonStatus,
+  selectStatus,
+  setCurrentPage,
+  setDisplaySettingsSaved,
+} from '../../Redux/Slices/facebookLoginWrapper/facebookLoginWrapperSlice';
+import { green } from '@mui/material/colors';
 
 const responseFacebook = (response: any) => {
   // TODO: Send access token for backend to do its work
-}
-
+};
 
 export function FacebookLoginWrapper() {
-  const facebook_app_id = process.env.REACT_APP_FACEBOOK_APP_ID ?? "";
+  const facebook_app_id = process.env.REACT_APP_FACEBOOK_APP_ID ?? '';
+
   let status = useAppSelector(selectStatus);
+  let saveButtonStatus = useAppSelector(selectSaveButtonStatus);
+  let displaySettingsSaved = useAppSelector(selectDisplaySettingsSaved);
+  let pages = useAppSelector(selectPages);
+  let currentPage = useAppSelector(selectCurrentPage);
   const dispatch = useAppDispatch();
 
-  useEffect(()=>{
-    dispatch(getActiveAsync());
-  },[dispatch]);
-  
-  let buttonStyle: React.CSSProperties = {};
+  useEffect(() => {
+    dispatch(getSettingsAsync());
+  }, [dispatch]);
+
+  let buttonStyle: React.CSSProperties = { width: '100%', height: '100%' };
   let buttonText = undefined;
   let buttonDisabled = false;
-  if (status === "loading") {
-    buttonText = "Loading status..."
-    buttonStyle = {cursor: "not-allowed"};
+  let selectDisabled = true;
+  if (status === 'loading') {
+    buttonText = 'Loading status...';
+    buttonStyle.cursor = 'not-allowed';
     buttonDisabled = true;
-  } else if (status === "loggedIn") {
-    buttonStyle = {backgroundColor: "green", borderColor: "green", cursor: "not-allowed"};
-    buttonText = "Logged in with Facebook"
+    selectDisabled = true;
+  } else if (status === 'loggedIn') {
+    buttonStyle = {
+      ...buttonStyle,
+      backgroundColor: 'green',
+      borderColor: 'green',
+      cursor: 'not-allowed',
+    };
+    buttonText = 'Logged in with Facebook';
     buttonDisabled = true;
+    selectDisabled = false;
   }
 
   return (
-    <FacebookLogin
-      appId={facebook_app_id}
-      fields="accounts"
-      scope="pages_show_list,pages_read_engagement,pages_read_user_content"
-      callback={responseFacebook}
-      size="small"
-      icon="fa-facebook"
-      isDisabled={buttonDisabled}
-      buttonStyle={buttonStyle}
-      textButton={buttonText} />
-  )
+    <Grid container spacing={2}>
+      <Grid item xs={4}>
+        <FacebookLogin
+          appId={facebook_app_id}
+          fields="accounts"
+          scope="pages_show_list,pages_read_engagement,pages_read_user_content"
+          callback={responseFacebook}
+          size="small"
+          icon="fa-facebook"
+          isDisabled={buttonDisabled}
+          buttonStyle={buttonStyle}
+          textButton={buttonText}
+        />
+      </Grid>
+
+      <Grid item xs={6}>
+        <FormControl fullWidth>
+          <InputLabel id="facebook-page-select-label">Facebook Page</InputLabel>
+          <Select
+            labelId="facebook-page-select-label"
+            id="facebook-page-select"
+            value={currentPage ?? ""}
+            label="Facebook Page"
+            onChange={e => dispatch(setCurrentPage(e.target.value))}
+            disabled={selectDisabled}
+          >
+            <MenuItem disabled value="">
+              <em>Select a Page</em>
+            </MenuItem>
+            {pages.map(p =>
+              <MenuItem key={p.value} value={p.value}>{p.name}</MenuItem>
+            )}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={2}>
+        <Button variant="contained" color="success" size='large' disabled={status !== "loggedIn"} onClick={() => dispatch(saveCurrentPageAsync(currentPage ?? ""))}>
+          <SaveIcon/>Save
+          {saveButtonStatus === 'loading' && (
+            <CircularProgress
+              size={24}
+              sx={{
+                color: green[500],
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                marginTop: '-12px',
+                marginLeft: '-12px',
+              }}
+            />
+          )}
+          </Button>
+      </Grid>
+
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={displaySettingsSaved}
+        autoHideDuration={2000}
+        onClose={() => dispatch(setDisplaySettingsSaved(false))}
+        key={"snackbarFacebookLoginWrapper"}>
+          <Alert severity="success" sx={{ width: '100%' }}>
+            Page setting has been saved successfully!
+          </Alert>
+        </Snackbar>
+    </Grid>
+  );
 }
--- frontend/app/src/Redux/Slices/facebookLoginWrapper/facebookLoginWrapperAPI.ts
+++ frontend/app/src/Redux/Slices/facebookLoginWrapper/facebookLoginWrapperAPI.ts
@@ -1,7 +1,17 @@
-// A mock function to mimic making an async request for data
-export function fetchActive() {
-  return new Promise<{ data: boolean }>((resolve) =>
-    setTimeout(() => resolve({ data: false }), 2000)
-    // TODO: Check if active
+export function fetchSettings() {
+  // TODO: Retrieve pages or null if no authentication
+  return new Promise<{ currentPage: string, pages: { name: string, value: string }[] } | null>((resolve) =>
+    // setTimeout(() => resolve(null), 2000)
+    setTimeout(() => resolve({
+      currentPage: "456",
+      pages: [{name: "Xperience", value: "123"}, {name: "Test", value: "456"}]
+    }), 2000)
+  );
+}
+
+export function saveCurrentPage(page: string) {
+  // TODO: Save current page
+  return new Promise<boolean>((resolve) => 
+    setTimeout(() => resolve(true), 2000)
   );
 }
