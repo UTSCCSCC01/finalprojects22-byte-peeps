0f5024240d279ab3b9748ba3b1807c7c08f05705
Bassel Ashi <passel.y@el-ashi.net>
2022-06-29 15:08:10 -0400

    Revert "BYT 51: sentiment analysis pie chart (#27)"
    This reverts commit a49890ce32dc055311e8eb1ee324ae53e3389fe0.

--- backend/app/src/controllers/facebook/comment.ts
+++ backend/app/src/controllers/facebook/comment.ts
@@ -1,6 +1,5 @@
 import { RequestHandler } from 'express';
 import FacebookComment from '../../models/facebook/comment';
-const { Op } = require("sequelize");
 
 /**
  * Provides the 50 most recent Facebook comments
@@ -53,62 +52,18 @@ export const getCommentsSentimentAnalysis: RequestHandler = async (
   res,
   next
 ) => {
-  const startDateParam = req.query.start;
-  const endDateParam = req.query.end;
-  
-  let startDate: Date;
-  let endDate: Date;
-
-  if (startDateParam && endDateParam) {
-    if (startDateParam.length === 8 && endDateParam.length === 8) {
-      // parse
-      const year = parseInt((startDateParam.toString()).substring(0, 4));
-      const month = parseInt((startDateParam.toString()).substring(4, 6));
-      const day = parseInt((startDateParam.toString()).substring(6, 8));
-
-      const year_end = parseInt((endDateParam.toString()).substring(0, 4));
-      const month_end = parseInt((endDateParam.toString()).substring(4, 6));
-      const day_end = parseInt((endDateParam.toString()).substring(6, 8));
-
-      try {
-        startDate = new Date(year, month - 1, day);
-        endDate = new Date(year_end, month_end - 1, day_end + 1);
-        console.log(startDate, endDate);
-        const positive = await FacebookComment.count({
-          where: { 
-            sentimentAnalysis: 'positive', 
-            date: {
-              [Op.between]: [startDate, endDate]
-            }
-          },
-        });
-        const neutral = await FacebookComment.count({
-          where: { 
-            sentimentAnalysis: 'neutral',
-            date: {
-              [Op.between]: [startDate, endDate]
-            }
-          },
-        });
-        const negative = await FacebookComment.count({
-          where: { 
-            sentimentAnalysis: 'negative', 
-            date: {
-              [Op.between]: [startDate, endDate]
-            }
-          },
-        });
-        res.send({
-          'positive': positive,
-          'neutral': neutral,
-          'negative': negative,
-        });
-      } catch (error) {
-        // throw new Error("Invalid Date Input");
-        res.status(404).send("Date Input not Provided");
-      }
-    }
-  }else {
-    res.status(404).send("Invalid Date Input");
-  }
+  const positive = await FacebookComment.count({
+    where: { sentimentAnalysis: 'positive' },
+  });
+  const neutral = await FacebookComment.count({
+    where: { sentimentAnalysis: 'neutral' },
+  });
+  const negative = await FacebookComment.count({
+    where: { sentimentAnalysis: 'negative' },
+  });
+  res.send({
+    'positive': positive,
+    'neutral': neutral,
+    'negative': negative,
+  });
 };
--- frontend/app/src/Redux/store.ts
+++ frontend/app/src/Redux/store.ts
@@ -2,14 +2,12 @@ import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from './Slices/counter/counterSlice';
 import facebookSetupReducer from './Slices/facebookSetup/facebookSetupSlice';
 import instagramSetupReducer from './Slices/instagramSetup/instagramSetupSlice';
-import facebookReducer from './Slices/facebook/facebookSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     facebookSetup: facebookSetupReducer,
-    instagramSetup: instagramSetupReducer,
-    facebook: facebookReducer
+    instagramSetup: instagramSetupReducer
   },
 });
 
