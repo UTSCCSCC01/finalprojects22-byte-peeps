089b331f0703571b2b55bbc0dad8866d4273b7c8
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-02 00:44:47 -0400

    BYT-84-updating routes

---
+++ frontend/app/src/RoutesConstants.tsx
@@ -0,0 +1,17 @@
+export enum RoutePaths {
+  SignIn = '/',
+  SignUp = '/signup',
+  Dashboard = '/dashboard',
+  Surveys = '/surveys',
+  Reviews = '/reviews',
+  Socials = '/socials',
+}
+
+export enum RouteNames {
+  SignIn = 'Sign In',
+  SignUp = 'Sign Up',
+  Dashboard = 'Dashboard',
+  Surveys = 'Surveys',
+  Reviews = 'Review Apps',
+  Socials = 'Social Media',
+}
