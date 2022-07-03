b50ac9bde35aa4a7712c381af391231043e362d6
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-02 20:26:16 -0400

    BYT-43-updated youtube routes

---
+++ backend/app/src/routes/youtube/video.ts
@@ -0,0 +1,19 @@
+import { Router } from 'express';
+import {
+  createVideo,
+  deleteVideo,
+  getAllVideos,
+  getVideoById,
+} from '../../controllers/youtube/video';
+
+const videoRouter = Router();
+
+videoRouter.post('/create', createVideo);
+
+videoRouter.get('/', getAllVideos);
+
+videoRouter.get('/:id', getVideoById);
+
+videoRouter.delete('/:id', deleteVideo);
+
+export default videoRouter;
