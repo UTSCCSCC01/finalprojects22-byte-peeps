a26d34c7b21c5cba2a9a3f11652f80cb51e8e417
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-02 20:00:49 -0400

    BYT-43-updating app.ts to schedule jobs

--- backend/app/src/app.ts
+++ backend/app/src/app.ts
@@ -8,7 +8,7 @@ import bodyParser from 'body-parser';
 import session from 'express-session';
 
 /* Routing imports */
-import userRoutes from './routes/user'
+import userRoutes from './routes/user';
 import instagramRoutes from './routes/instagram/routes';
 import facebookRoutes from './routes/facebook/routes';
 import setupRoutes from './routes/setup/routes';
@@ -17,12 +17,18 @@ import setupRoutes from './routes/setup/routes';
 import { instagramScheduledJob } from './dataPipelines/instagram';
 import { facebookScheduledJob } from './dataPipelines/facebook';
 import authenticateUser from './middlewares/validateAuth';
+import { youtubeScheduledJob } from './dataPipelines/youtube';
 
 const app = express();
 const cors = require('cors');
 const PORT = process.env.BACKEND_PORT;
 
-app.use(cors({ origin: `http://localhost:${process.env.FRONTEND_PORT}`, credentials: true }));
+app.use(
+  cors({
+    origin: `http://localhost:${process.env.FRONTEND_PORT}`,
+    credentials: true,
+  })
+);
 app.use(
   session({
     secret: 'please change this secret',
@@ -48,14 +54,14 @@ declare module 'express-session' {
 }
 
 /* User Routes */
-app.use("/user", userRoutes);
+app.use('/user', userRoutes);
 
 /* Social Media Routing */
 app.use('/instagram', instagramRoutes);
 app.use('/facebook', facebookRoutes);
 
 /* Setup Routing */
-app.use("/setup", authenticateUser, setupRoutes);
+app.use('/setup', authenticateUser, setupRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -77,3 +83,4 @@ app.listen(PORT, () => {
 /* Cron Jobs */
 instagramScheduledJob.start();
 facebookScheduledJob.start();
+youtubeScheduledJob.start();
