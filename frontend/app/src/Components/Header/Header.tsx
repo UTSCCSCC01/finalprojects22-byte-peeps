26142e106e3ec0a98ed5e3cf099abb5be3245e6a
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-08 18:37:38 -0400

    BYT-136: cleaned up header component

--- frontend/app/src/Components/Header/Header.tsx
+++ frontend/app/src/Components/Header/Header.tsx
@@ -1,104 +1,38 @@
-import AppsIcon from '@mui/icons-material/Apps';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import ListAltIcon from '@mui/icons-material/ListAlt';
-import MenuIcon from '@mui/icons-material/Menu';
-import PeopleIcon from '@mui/icons-material/People';
-import SettingsIcon from '@mui/icons-material/Settings';
-import {
-  AppBar,
-  Box,
-  Drawer,
-  IconButton,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from '@mui/material';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useAppSelector } from '../../Redux/hooks';
-import {
-  selectSignInStatus,
-  selectUserName,
-} from '../../Redux/Slices/user/userSlice';
-import { RouteNames, RoutePaths } from '../Router/RoutesConstants';
+import { AppBar, Box, Toolbar } from '@mui/material';
+import DateSelector from '../DateSelector/DateSelector';
+import IsSignedIn from '../IsSignedIn/IsSignedIn';
+import Logo from '../Logo/Logo';
+import PageName from '../PageName/PageName';
+import SideNav from '../SideNav/SideNav';
 import SignOutBtn from '../SignOutBtn/SignOutBtn';
+import UserInfo from '../UserInfo/UserInfo';
 import './Header.css';
 
-const Pages = [
-  {
-    name: RouteNames.Dashboard,
-    icon: <HomeOutlinedIcon />,
-    link: RoutePaths.Dashboard,
-  },
-  { name: RouteNames.Surveys, icon: <ListAltIcon />, link: RoutePaths.Surveys },
-  { name: RouteNames.Reviews, icon: <AppsIcon />, link: RoutePaths.Reviews },
-  { name: RouteNames.Socials, icon: <PeopleIcon />, link: RoutePaths.Socials },
-  {
-    name: RouteNames.Settings,
-    icon: <SettingsIcon />,
-    link: RoutePaths.Settings,
-  },
-];
+interface Props {}
 
-export default function Header() {
-  const [open, setOpen] = useState<boolean>(false);
-
-  const navigate = useNavigate();
-
-  const userSignedIn: boolean = useAppSelector(selectSignInStatus);
-  const username: string = useAppSelector(selectUserName);
-
-  const getList = () => (
-    <div style={{ width: 250 }} onClick={() => setOpen(true)}>
-      {Pages.map((item, index, link) => (
-        <ListItem button key={index} onClick={() => navigate(item.link)}>
-          <ListItemIcon>{item.icon}</ListItemIcon>
-          <ListItemText primary={item.name} />
-        </ListItem>
-      ))}
-    </div>
-  );
-
+const Header: React.FC<Props> = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar variant="dense">
-          {userSignedIn && (
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              onClick={() => setOpen(true)}
-            >
-              <MenuIcon />
-            </IconButton>
-          )}
-          <Drawer
-            open={open}
-            variant="temporary"
-            anchor={'left'}
-            onClose={() => setOpen(false)}
-          >
-            {getList()}
-          </Drawer>
-          <Typography variant="h6" color="inherit" component="div">
-            <img
-              src="/images/logo-white.png"
-              alt="logo"
-              style={{ width: '155px', marginTop: '7px' }}
-            />
-          </Typography>
-
-          {userSignedIn && (
-            <div className="userInfo">
-              <div className="username">{username}</div>
-              <SignOutBtn></SignOutBtn>
-            </div>
-          )}
+        <Toolbar variant="dense" className="toolBar">
+          <IsSignedIn>
+            <SideNav />
+          </IsSignedIn>
+          <Logo />
+          <IsSignedIn>
+            <PageName />
+          </IsSignedIn>
+          <div className="rightComponent">
+            <IsSignedIn>
+              <DateSelector />
+              <UserInfo className="userInfo" />
+              <SignOutBtn />
+            </IsSignedIn>
+          </div>
         </Toolbar>
       </AppBar>
     </Box>
   );
-}
+};
+
+export default Header;
