5e92807beb4b83195b388dcca2649a0931ce1277
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-08 18:15:34 -0400

    BYT-136: cleaned up redux store

---
+++ frontend/app/src/Redux/Slices/global/globalReduxConstants.ts
@@ -0,0 +1,9 @@
+export enum AppNames {
+  GoogleReviews = 'Google Reviews',
+  Yelp = 'Yelp',
+  Instagram = 'Instagram',
+  Facebook = 'Facebook',
+  Twitter = 'Twitter',
+  Reddit = 'Reddit',
+  YouTube = 'YouTube',
+}
