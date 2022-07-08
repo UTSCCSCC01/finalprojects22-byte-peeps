22a273657717fc4b73f39df10073d7a49c8024da
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-08 18:38:20 -0400

    BYT-136: cleaned up redux store

--- frontend/app/src/Redux/reduxConstants.ts
+++ frontend/app/src/Redux/reduxConstants.ts
@@ -4,16 +4,3 @@ export enum ReduxStatus {
   loading = 'loading',
   idle = 'idle',
 }
-
-export enum ResponseStatus {
-  success = 200 | 201,
-  badRequest = 400,
-  unauthorized = 401,
-  forbidden = 403,
-  notFound = 404,
-  serverError = 500 | 504,
-}
-
-export type ErrorResponse = {
-  message: string;
-};
