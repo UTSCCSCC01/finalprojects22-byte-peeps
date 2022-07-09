c0a47083b86e2389e3c425085b9047cd4cc3d7c8
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-08 22:02:37 -0400

    BYT-136: removed duplicate pages

--- frontend/app/src/pages/Dashboard/Dashboard.tsx
+++ frontend/app/src/pages/Dashboard/Dashboard.tsx
@@ -3,8 +3,6 @@ import { RouteNames } from '../../Components/Router/RoutesConstants';
 import { useAppDispatch } from '../../Redux/hooks';
 import { setPageName } from '../../Redux/Slices/global/globalSlice';
 
-import './Dashboard.css';
-
 interface Props {}
 
 const Dashboard: React.FunctionComponent<Props> = (props) => {
