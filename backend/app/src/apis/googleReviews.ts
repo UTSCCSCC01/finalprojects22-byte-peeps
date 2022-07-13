8eda476efe5b83f136a47164e72536f0aea51103
Bassel Ashi <passel.y@el-ashi.net>
2022-07-13 00:47:37 -0400

    BYT-146: Define viewmodels and implement date filtering

--- backend/app/src/apis/googleReviews.ts
+++ backend/app/src/apis/googleReviews.ts
@@ -8,14 +8,20 @@ import axios from 'axios';
     - https://developers.google.com/my-business/reference/rest/v4/accounts.locations.reviews#Review
 */
 
-type GoogleReview = {
+export type GoogleReviewWrapperViewModel = {
+  totalReviewCount: number;
+  averageRating: number;
+  reviews: GoogleReviewViewModel[];
+};
+
+export type GoogleReviewViewModel = {
   id: string;
   title: string;
   review: string;
   reviewer: string | null;
   rating: number | null;
   date: Date;
-  reply: string | null;
+  response: string | null;
 };
 
 const BASE_URL = 'https://mybusiness.googleapis.com/v4';
@@ -28,16 +34,23 @@ RATING_ENUMS.THREE = 3;
 RATING_ENUMS.FOUR = 4;
 RATING_ENUMS.FIVE = 5;
 
-export const getAllReviews = async (
+/**
+ * Retrieved general data of all reviews in addition to filtered reviews
+ * @param bearerToken OAuth 2.0 token for connecting to the API
+ * @param accountId Google Business Account ID
+ * @param locationId Location ID to retrieve data from
+ * @param startDate left bound date for reviews to be retrieved
+ * @param endDate right bound date for reviews to be retrieved
+ * @returns a GoogleReviewWrapper containing aggregated data plus filtered reviews
+ */
+export const getReviews = async (
   bearerToken: string,
   accountId: string,
-  locationId: string
-): Promise<{
-  totalReviewCount: number;
-  averageRating: number;
-  reviews: GoogleReview[];
-}> => {
-  const reviews: GoogleReview[] = [];
+  locationId: string,
+  startDate: Date,
+  endDate: Date
+): Promise<GoogleReviewWrapperViewModel> => {
+  const reviews: GoogleReviewViewModel[] = [];
   let averageRating = 0;
   let totalReviewCount = 0;
 
@@ -45,7 +58,7 @@ export const getAllReviews = async (
   let pageToken: string | undefined | null = null;
 
   while (pageToken != undefined) {
-    const params: any = { pageSize: 50 };
+    const params: any = { pageSize: 50, orderBy: 'updateTime desc' };
     if (pageToken) params.pageToken = pageToken; // for first call to not have pageToken
 
     const result = await axios
@@ -57,17 +70,13 @@ export const getAllReviews = async (
 
     if (!result) break;
 
-    reviews.push(
-      ...result!.data.reviews.map((r: any) => {
-        r.reviewId;
-        r.name;
-        r.comment;
-        r.reviewer.displayName == '' ? null : r.reviewer.displayName;
-        RATING_ENUMS[r.starRating];
-        new Date(r.reviewer.updateTime);
-        r.reviewReply?.comment ?? null;
-      })
+    const currentPageReviews = result!.data.reviews.map((r: any) =>
+      mapReviewsResponseToType(r)
     );
+    reviews.push(
+      ...filterReviewsByDate(currentPageReviews, startDate, endDate)
+    );
+
     if (!pageToken) {
       // add these values only in first iteration
       totalReviewCount = result!.data.totalReviewCount;
@@ -76,4 +85,36 @@ export const getAllReviews = async (
     pageToken = result!.data.nextPageToken;
   }
   return { totalReviewCount, averageRating, reviews };
+};
+
+/**
+ * Maps response reviews object to a GoogleReview type
+ * @param review an any object that contains specific fields defined in the third link at the top of the document
+ * @returns a GoogleReview object
+ */
+const mapReviewsResponseToType = (review: any): GoogleReviewViewModel => {
+  return {
+    id: review.reviewId,
+    title: review.name,
+    review: review.comment,
+    reviewer:
+      review.reviewer.displayName == '' ? null : review.reviewer.displayName,
+    rating: RATING_ENUMS[review.starRating],
+    date: new Date(review.reviewer.updateTime),
+    response: review.reviewReply?.comment ?? null,
+  };
+};
+
+/**
+ * Returns a subset of the reviews provided that are within startDate and endDate
+ * @param reviews list of GoogleReview objects
+ * @param startDate left interval for dates
+ * @param endDate right interval for dates
+ */
+const filterReviewsByDate = (
+  reviews: GoogleReviewViewModel[],
+  startDate: Date,
+  endDate: Date
+): GoogleReviewViewModel[] => {
+  return reviews.filter((r) => r.date > startDate && r.date < endDate);
 };
