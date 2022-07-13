bd2c1a2786ec5097a30a5c455f0491f41f50e9a0
Bassel Ashi <passel.y@el-ashi.net>
2022-07-12 22:32:34 -0400

    BYT-146: Create rating field in review with validation

--- backend/app/src/models/googleReviews/review.ts
+++ backend/app/src/models/googleReviews/review.ts
@@ -5,10 +5,13 @@ import {
   DataType,
   DeletedAt,
   ForeignKey,
+  Is,
   Model,
   Table,
 } from 'sequelize-typescript';
 import GoogleReviewsApi from './api';
+
+const RATING_REGEX = /^[1-5]$/;
 
 @Table({
   tableName: 'google_reviews_reviews',
@@ -25,6 +28,16 @@ export default class GoogleReviewsReview extends Model {
   // Can be null if reviewer is anonymous
   @Column
   reviewer: string;
+
+  // Can be unspecified
+  @Is('ValidRating', (value) => {
+    if (!RATING_REGEX.test(value))
+      throw new Error(
+        'Invalid rating specified, value needs to be from 1 to 5 or null'
+      );
+  })
+  @Column
+  rating: number;
 
   @Column
   response: string;
