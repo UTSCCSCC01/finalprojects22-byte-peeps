a0c2f389fbf766a25a0f8434473f799342335fb1
Bassel Ashi <passel.y@el-ashi.net>
2022-07-13 23:19:34 -0400

    BYT-170: Fix filter field for getComments

--- backend/app/src/controllers/reddit/comment.ts
+++ backend/app/src/controllers/reddit/comment.ts
@@ -1,21 +1,28 @@
-import { RequestHandler } from "express";
-import { unknownError } from "../../globalHelpers/globalConstants";
-import RedditComment from "../../models/reddit/comment";
-import RedditListing from "../../models/reddit/listing";
-import RedditSubreddit from "../../models/reddit/subreddit";
+import { RequestHandler } from 'express';
+import { unknownError } from '../../globalHelpers/globalConstants';
+import RedditComment from '../../models/reddit/comment';
+import RedditListing from '../../models/reddit/listing';
+import RedditSubreddit from '../../models/reddit/subreddit';
 const { Op } = require('sequelize');
-import User from "../../models/user/user";
+import User from '../../models/user/user';
 
 /**
  * Provides the page number and size, provides comments of any Reddit subreddit related to the user API
  */
 export const getComments: RequestHandler = async (req, res, next) => {
   try {
-    if (!req.query.startDate || req.query.startDate.length !== 8
-      || !req.query.endDate || req.query.endDate.length !== 8)
+    if (
+      !req.query.startDate ||
+      req.query.startDate.length !== 8 ||
+      !req.query.endDate ||
+      req.query.endDate.length !== 8
+    )
       return res.status(400).send();
 
-    const user = await User.findOne({ where: { username: req.session.username }, include: RedditSubreddit });
+    const user = await User.findOne({
+      where: { username: req.session.username },
+      include: RedditSubreddit,
+    });
     const pageNumber = parseInt(req.query.page?.toString() ?? '0');
     const pageSize = parseInt(req.query.pageSize?.toString() ?? '0');
 
@@ -31,21 +38,20 @@ export const getComments: RequestHandler = async (req, res, next) => {
     const endDay = parseInt(endDateParam.toString().substring(6, 8));
     const endDate = new Date(endYear, endMonth - 1, endDay + 1);
 
-    if (!user?.subreddit)
-      return res.send({ count: 0, data: [] });
+    if (!user?.subreddit) return res.send({ count: 0, data: [] });
 
-    const listings = await RedditListing.findAll({ where: { listingId: user!.subreddit.id } });
-    const listingIds: number[] = listings.map(l => l.id);
+    const listings = await RedditListing.findAll({
+      where: { subredditId: user!.subreddit.id },
+    });
+    const listingIds: number[] = listings.map((l) => l.id);
     const comments = await RedditComment.findAll({
       where: {
         listingId: listingIds,
         date: {
           [Op.between]: [startDate, endDate],
-        }
+        },
       },
-      order: [
-        ['date', 'DESC']
-      ],
+      order: [['date', 'DESC']],
       attributes: [
         'id',
         'text',
@@ -53,10 +59,13 @@ export const getComments: RequestHandler = async (req, res, next) => {
         'replies',
         'sentimentAnalysis',
         'topicClassification',
-        'subjectivityAnalysis'
-      ]
+        'subjectivityAnalysis',
+      ],
     });
-    const filteredComments = comments.slice(pageNumber * pageSize, pageNumber * pageSize + pageSize);
+    const filteredComments = comments.slice(
+      pageNumber * pageSize,
+      pageNumber * pageSize + pageSize
+    );
     res.send({ count: comments.length, data: filteredComments });
   } catch (e) {
     console.log(e);
