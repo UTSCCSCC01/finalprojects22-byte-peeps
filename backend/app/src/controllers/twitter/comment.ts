686a98c2dbf3329cd323ca26b9aadcaf8d71dd63
Bassel Ashi <passel.y@el-ashi.net>
2022-07-13 23:23:18 -0400

    BYT-170: Hotfix controller comment field (#53)
    * BYT-170: Fix filter field for getComments
    
    * BYT-170: Fix filter field for getComments

--- backend/app/src/controllers/twitter/comment.ts
+++ backend/app/src/controllers/twitter/comment.ts
@@ -1,21 +1,28 @@
-import { RequestHandler } from "express";
-import { unknownError } from "../../globalHelpers/globalConstants";
+import { RequestHandler } from 'express';
+import { unknownError } from '../../globalHelpers/globalConstants';
 const { Op } = require('sequelize');
-import TwitterConversation from "../../models/twitter/conversation";
-import TwitterTweet from "../../models/twitter/tweet";
-import TwitterUser from "../../models/twitter/user";
-import User from "../../models/user/user";
+import TwitterConversation from '../../models/twitter/conversation';
+import TwitterTweet from '../../models/twitter/tweet';
+import TwitterUser from '../../models/twitter/user';
+import User from '../../models/user/user';
 
 /**
  * Provides the page number and size, provides comments of any Twitter tweet related to the user API
  */
- export const getComments: RequestHandler = async (req, res, next) => {
+export const getComments: RequestHandler = async (req, res, next) => {
   try {
-    if (!req.query.startDate || req.query.startDate.length !== 8 
-      || !req.query.endDate || req.query.endDate.length !== 8)
+    if (
+      !req.query.startDate ||
+      req.query.startDate.length !== 8 ||
+      !req.query.endDate ||
+      req.query.endDate.length !== 8
+    )
       return res.status(400).send();
-    
-    const user = await User.findOne({where: { username: req.session.username }, include: TwitterUser});
+
+    const user = await User.findOne({
+      where: { username: req.session.username },
+      include: TwitterUser,
+    });
     const pageNumber = parseInt(req.query.page?.toString() ?? '0');
     const pageSize = parseInt(req.query.pageSize?.toString() ?? '0');
 
@@ -31,21 +38,20 @@ import User from "../../models/user/user";
     const endDay = parseInt(endDateParam.toString().substring(6, 8));
     const endDate = new Date(endYear, endMonth - 1, endDay + 1);
 
-    if (!user?.twitterUser)
-      return res.send({ count: 0, data: [] });
+    if (!user?.twitterUser) return res.send({ count: 0, data: [] });
 
-    const tweets = await TwitterTweet.findAll({ where: { apiId: user!.twitterUser.id }});
-    const tweetIds: number[] = tweets.map(p => p.id);
+    const tweets = await TwitterTweet.findAll({
+      where: { twitterUserId: user!.twitterUser.id },
+    });
+    const tweetIds: number[] = tweets.map((p) => p.id);
     const comments = await TwitterConversation.findAll({
       where: {
         tweetId: tweetIds,
         date: {
           [Op.between]: [startDate, endDate],
-        }
+        },
       },
-      order: [
-        ['date', 'DESC']
-      ],
+      order: [['date', 'DESC']],
       attributes: [
         'id',
         'text',
@@ -54,12 +60,15 @@ import User from "../../models/user/user";
         'likes',
         'sentimentAnalysis',
         'topicClassification',
-        'subjectivityAnalysis'
-      ]
+        'subjectivityAnalysis',
+      ],
     });
-    const filteredComments = comments.slice(pageNumber * pageSize, pageNumber * pageSize + pageSize);
+    const filteredComments = comments.slice(
+      pageNumber * pageSize,
+      pageNumber * pageSize + pageSize
+    );
     res.send({ count: comments.length, data: filteredComments });
-  } catch(e) {
+  } catch (e) {
     console.log(e);
     res.status(500).json({ message: unknownError });
   }
