d56dc8670b71e2c55fc160bc77d0944ca43235b6
Karandeep Lubana <karandeeplubana1@gmail.com>
2022-07-14 22:37:16 -0400

    BYT-50: center loader & add percent values

--- frontend/app/src/Components/Charts/PieChart/PieChartAnalysis.tsx
+++ frontend/app/src/Components/Charts/PieChart/PieChartAnalysis.tsx
@@ -6,7 +6,7 @@ import './PieChartAnalysis.css';
 
 interface SeriesData {
   name: String;
-  value: Number;
+  value: number;
 }
 
 export interface PieChartAnalysisProps {
@@ -22,55 +22,65 @@ interface PieChartComponentProps {
   COLORS: string[];
 }
 
-const PieChartComponent = ({ data, COLORS }: PieChartComponentProps) => (
-  <ResponsiveContainer width="95%" height={260}>
-    <PieChart>
-      <Pie
-        dataKey="value"
-        data={data}
-        label={({
-          cx,
-          cy,
-          midAngle,
-          innerRadius,
-          outerRadius,
-          value,
-          index,
-        }) => {
-          const RADIAN = Math.PI / 180;
-          const radius = 25 + innerRadius + (outerRadius - innerRadius);
-          const x = cx + radius * Math.cos(-midAngle * RADIAN);
-          const y = cy + radius * Math.sin(-midAngle * RADIAN);
+const PieChartComponent = ({ data, COLORS }: PieChartComponentProps) => {
+  const total = data.reduce((accum, obj) => {
+    return accum + obj.value;
+  }, 0);
 
-          return value !== 0 ? (
-            <text
-              style={{ fontSize: '0.6rem' }}
-              x={x}
-              y={y}
-              fill={COLORS[index]}
-              textAnchor={x > cx ? 'start' : 'end'}
-              dominantBaseline="central"
-            >
-              {`${data[index].name} (${value})`}
-            </text>
-          ) : null;
-        }}
-      >
-        {data.map((entry, index) =>
-          entry.value !== 0 ? (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-          ) : (
-            <Cell
-              display="none"
-              key={`cell-${index}`}
-              fill={COLORS[index % COLORS.length]}
-            />
-          )
-        )}
-      </Pie>
-    </PieChart>
-  </ResponsiveContainer>
-);
+  // console.log(result);total
+  return (
+    <ResponsiveContainer width="95%" height={260}>
+      <PieChart>
+        <Pie
+          dataKey="value"
+          data={data}
+          label={({
+            cx,
+            cy,
+            midAngle,
+            innerRadius,
+            outerRadius,
+            value,
+            index,
+          }) => {
+            const RADIAN = Math.PI / 180;
+            const radius = 25 + innerRadius + (outerRadius - innerRadius);
+            const x = cx + radius * Math.cos(-midAngle * RADIAN);
+            const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+            return value !== 0 ? (
+              <text
+                style={{ fontSize: '0.6rem' }}
+                x={x}
+                y={y}
+                fill={COLORS[index]}
+                textAnchor={x > cx ? 'start' : 'end'}
+                dominantBaseline="central"
+              >
+                {`${data[index].name} ${(value / total * 100).toFixed(1)}%`}
+              </text>
+            ) : null;
+          }}
+        >
+          {data.map((entry, index) =>
+            entry.value !== 0 ? (
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ) : (
+              <Cell
+                display="none"
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            )
+          )}
+        </Pie>
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
 
 const PieChartAnalysis = ({
   COLORS,
