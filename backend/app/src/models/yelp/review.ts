5d7fe3cffd008184b72abad1370ec9ce4a883020
Mohamed Tayeh <35163331+mohamed-tayeh@users.noreply.github.com>
2022-07-22 17:04:59 -0400

    BYT-191: updated yelp models

--- backend/app/src/models/yelp/review.ts
+++ backend/app/src/models/yelp/review.ts
@@ -3,13 +3,15 @@ import {
   BelongsTo,
   Column,
   DataType,
-  Default,
   DeletedAt,
   ForeignKey,
+  Is,
   Model,
   Table,
 } from 'sequelize-typescript';
 import YelpBusiness from './business';
+
+const RATING_REGEX = /^[1-5]$/;
 
 @Table({
   tableName: 'yelp_reviews',
@@ -19,7 +21,13 @@ export default class YelpReview extends Model {
   @Column
   dataId: string;
 
-  @AllowNull(false)
+  // Can be unspecified
+  @Is('ValidRating', (value) => {
+    if (!RATING_REGEX.test(value))
+      throw new Error(
+        'Invalid rating specified, value needs to be from 1 to 5 or null'
+      );
+  })
   @Column
   rating: number;
 
