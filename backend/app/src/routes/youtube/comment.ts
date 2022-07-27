79c590617d13f816c27ae0aeacf9546943a0f206
Bassel Ashi <passel.y@el-ashi.net>
2022-07-27 16:41:56 -0400

    BYT-185: Integrate middleware for table filtering

--- backend/app/src/routes/youtube/comment.ts
+++ backend/app/src/routes/youtube/comment.ts
@@ -5,12 +5,13 @@ import {
   getCommentsSubjectivityAnalysis,
   getWordCloudData,
 } from '../../controllers/youtube/comment';
+import tableFilter from '../../middlewares/tableFilter';
 
 const commentRouter = Router();
 
 commentRouter.get('/subjectivity_analysis', getCommentsSubjectivityAnalysis);
 commentRouter.get('/sentiment_analysis', getCommentsSentimentAnalysis);
-commentRouter.get('/', getComments);
+commentRouter.get('/', tableFilter, getComments);
 commentRouter.get('/wordCloud', getWordCloudData);
 
 export default commentRouter;
