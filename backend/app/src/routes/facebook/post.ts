6489822c256ed9be5701e82f66108b40898b23a2
Jiaming Yang <jiamingyang11@gmail.com>
2022-07-28 12:01:32 -0400

    BYT-232 subjectivity analysis routes

--- backend/app/src/routes/facebook/post.ts
+++ backend/app/src/routes/facebook/post.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import {
   getPosts,
-  getSentimentAnalysisForTimeSeries,
+  getSentimentAnalysisForTimeSeries, getSubjectivityAnalysisForTimeSeries
 } from '../../controllers/facebook/post';
 
 const postRouter = Router();
 
 postRouter.get('/', getPosts);
 postRouter.get('/sentiment_analysis', getSentimentAnalysisForTimeSeries);
+postRouter.get('/subjectivity_analysis', getSubjectivityAnalysisForTimeSeries)
 
 export default postRouter;
