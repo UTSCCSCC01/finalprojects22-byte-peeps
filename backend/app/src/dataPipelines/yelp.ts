b416daa72459a482d48d2d4b8ea7150150345005
Jiaming Yang <jiamingyang11@gmail.com>
2022-07-28 12:38:00 -0400

    BYT-231 yelp pipeline

---
+++ backend/app/src/dataPipelines/yelp.ts
@@ -0,0 +1,59 @@
+
+import YelpBusiness from '../models/yelp/business';
+import YelpReview from '../models/yelp/review';
+import axios from 'axios';
+/**
+ * Updates the database as follows:
+ *    1. Gets all yelp businessId stored in the db
+ *    2. For each businessId:
+ *        a. Fetches all comments created on the previous day.
+ **/
+export async function startPipeline() {
+    try {
+        // Get stored yelp businesses
+        let businesses = await YelpBusiness.findAll();
+
+        if (businesses.length == 0) return;
+
+        // Update data for each review
+        for (let i = 0; i < businesses.length; i++) {
+            await updateComment(businesses[i]);
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+const updateComment = async (business: YelpBusiness) => {
+    const initialUrl = 'https://www.yelp.com/biz/' + business.id + '/review_feed?rl=en&start=0';
+    try {
+        const response = await axios
+            .get(initialUrl)
+        let data = await response.data;
+        console.log(data)
+        if (data['reviews'] === undefined || data['reviews'].length == 0) return;
+
+        data['reviews'].forEach(
+            async (review: { [key: string]: any }) => {
+                try {
+                    await YelpReview.findOrCreate({
+                        where: {
+                            dataId: review['id'],
+                        },
+                        defaults: {
+                            rating: review['rating'],
+                            text: review['text'],
+                            date: review['time_created'],
+                            userName: review['user']['name'],
+                            businessId: business.id
+                        }
+                    })
+                } catch (err) {
+                    console.error(err);
+                }
+
+            }
+        )
+    } catch (err) {
+        console.error(err);
+    }
+}
